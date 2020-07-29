9bce52c1eca83e93352997b65fd7917495bd9f56
Oskar Radon <oskarradonkimball@gmail.com>
2020-07-28 17:20:56 -0700

    data section now working, modules no longer necessary

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
-import dateFormat from 'modules/dateformat.js';
+function updateTime(){
+  var today = new Date();
+  var hr = today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  var ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+  hr = (hr == 0) ? 12 : hr;
+  hr = (hr > 12) ? hr - 12 : hr;
 
-var now = new Date();
-var formattedTime = dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var curWeekDay = days[today.getDay()];
+  var curDay = today.getDate();
+  var curMonth = months[today.getMonth()];
+  var curYear = today.getFullYear();
+  var date = curWeekDay+", "+curMonth+" "+curDay+" "+curYear;
 
-console.log(formattedTime);
+  document.getElementById("clock").innerHTML = date + "<br>" + hr + ":" + min + ":" + sec + " " + ap;
+}
+setInterval(updateTime, 500);
