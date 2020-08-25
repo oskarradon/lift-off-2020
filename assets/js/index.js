2eddc995a24c68ec5d2b63a061d2372bd0b65f46
Oskar Radon <oskarradonkimball@gmail.com>
2020-08-25 12:40:45 -0700

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,44 @@
-let tl1 = gsap.timeline(),
-    tl2 = gsap.timeline();
+document.addEventListener("DOMContentLoaded", function() {
 
-tl1.from("#launching", {duration: 2, x: -100});
-tl1.from("#next", {duration: 2, x: 800}, .25);
-tl1.from("#designers", {duration: 2, x: -800}, .25);
-tl1.from("#and", {duration: 2, x: -1200}, .25);
-tl1.from("#illustrators", {duration: 2, x: -400}, .25);
+  let tl1 = gsap.timeline({defaults: {delay: .5, duration: 2, opacity: 0}}),
+      tl2 = gsap.timeline({defaults: {delay: .5, duration: 2, opacity: 0}});
 
+  tl1.from("#launching", {x: -100})
+     .from("#next", {x: 800}, .25)
+     .from("#designers", {x: -800}, .25)
+     .from("#and", {x: -1200}, .25)
+     .from("#illustrators", {x: -400}, .25)
+     .to("#text-container", {
+        motionPath: {
+          path: [{x:0, y:10}, {x:10, y:0}, {x:0, y:-10}, {x:-10, y:0}],
+          // align: "self",
+          // alignOrigin: [0.5, 0.5],
+          autoRotate: false
+        },
+        inherit:false,
+        duration: 5,
+        yoyo: true,
+        repeat: -1
+      });
+     // .to({},2,{});
 
-tl2.from("#bar1", {duration: 2, x: -1200});
-tl2.from("#bar2", {duration: 2, x: -800}, .25);
-tl2.from("#bar3", {duration: 2, x: 200}, .25);
-tl2.from("#bar4", {duration: 2, x: -800}, .25);
-tl2.from("#bar5", {duration: 2, x: 400}, .25);
+  tl2.from("#bar1", {x: 1200})
+     .from("#bar2", {x: -800}, .25)
+     .from("#bar3", {x: 200}, .25)
+     .from("#bar4", {x: 800}, .25)
+     .from("#bar5", {x: 400}, .25)
+     .to("#bars", {
+        motionPath: {
+          path: [{x:0, y:10}, {x:10, y:0}, {x:0, y:-10}, {x:-10, y:0}],
+          // align: "self",
+          // alignOrigin: [0.5, 0.5],
+          autoRotate: false
+        },
+        inherit:false,
+        duration: 5,
+        yoyo: true,
+        repeat: -1,
+        delay: 1
+      });
+     // .to({},2,{});
+});
