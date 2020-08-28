8e268cd7ef4690fac141f58ea8099d9678ff91b9
Oskar Radon <oskarradonkimball@gmail.com>
2020-08-27 18:40:07 -0700

    remove shake animation, add moving bg anim

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,37 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
      .from("#designers", {x: -800}, .25)
      .from("#and", {x: -1200}, .25)
      .from("#illustrators", {x: -400}, .25)
-     .to("#text-container", {
-        motionPath: {
-          path: [{x:0, y:10}, {x:10, y:0}, {x:0, y:-10}, {x:-10, y:0}],
-          // align: "self",
-          // alignOrigin: [0.5, 0.5],
-          autoRotate: false
-        },
-        inherit:false,
-        duration: 5,
-        yoyo: true,
-        repeat: -1
-      });
-     // .to({},2,{});
 
   tl2.from("#bar1", {x: 1200})
      .from("#bar2", {x: -800}, .25)
      .from("#bar3", {x: 200}, .25)
      .from("#bar4", {x: 800}, .25)
      .from("#bar5", {x: 400}, .25)
-     .to("#bars", {
-        motionPath: {
-          path: [{x:0, y:10}, {x:10, y:0}, {x:0, y:-10}, {x:-10, y:0}],
-          // align: "self",
-          // alignOrigin: [0.5, 0.5],
-          autoRotate: false
-        },
-        inherit:false,
-        duration: 5,
-        yoyo: true,
-        repeat: -1,
-        delay: 1
-      });
-     // .to({},2,{});
+
+gsap.from("#bar1", {backgroundPosition: "2247px 0px", duration: 30, ease: Linear.easeNone, repeat: -1})
+gsap.from("#bar2", {backgroundPosition: "-2247px 2000px", duration: 30, ease: Linear.easeNone, repeat: -1})
+gsap.from("#bar3", {backgroundPosition: "4000px 0px", duration: 30, ease: Linear.easeNone, repeat: -1})
+gsap.from("#bar4", {backgroundPosition: "-2247px 1000px", duration: 30, ease: Linear.easeNone, repeat: -1})
+gsap.from("#bar5", {backgroundPosition: "2247px 2000px", duration: 30, ease: Linear.easeNone, repeat: -1})
+
 });
